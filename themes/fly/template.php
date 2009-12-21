1b50661fbe15c796df8823cd8235fc39c36eea6b
Victoria Chan <contact@victoriac.net>
2009-12-21 19:56:40 +0000

    theming user pages

--- themes/fly/template.php
+++ themes/fly/template.php
@@ -6,7 +6,7 @@
  */
 
 // Format a date field to nice blog-style date
-function _make_blog_date($datefield) {
+function _fly_make_blog_date($datefield) {
   
   $arr_created_date_parts = explode(' ', format_date($datefield, 'small'));
   $ret  = '<p class="blog_date">';
@@ -19,7 +19,7 @@ function _make_blog_date($datefield) {
 }
 
 // Return a avatar thumb with link
-function _make_avatar_thumb($username, $userpicture) {
+function _fly_make_avatar_thumb($username, $userpicture) {
   
   $tmp_link = 'user/'.$username;
   if($userpicture){
@@ -32,10 +32,21 @@ function _make_avatar_thumb($username, $userpicture) {
   return $ret;
 }
 
+function _fly_removetab($label, &$vars) {
+  $tabs = explode("\n", $vars['tabs']);
+  $vars['tabs'] = '';
+
+  foreach ($tabs as $tab) {
+    if (strpos($tab, '>' . $label . '<') === FALSE) {
+      $vars['tabs'] .= $tab . "\n";
+    }
+  }
+}
 
 /**
- * Preprocess page template variables.
+ * Preprocess page templates
  */
+
 function fly_preprocess(&$vars, $hook) {
   
   if($hook == 'page') {
@@ -43,15 +54,22 @@ function fly_preprocess(&$vars, $hook) {
     if (isset($vars['node']) && $vars['node']->type) {
       $vars['body_classes'] = $vars['body_classes']. ' page-node';
     }
+        
+    // Remove user 'track' tab for non-admin
+    global $user;
+    if (!in_array('administrator', array_values($user->roles))) {
+      _fly_removetab('Track', $vars);
+    }
+    
+    _fly_removetab('Notification settings', $vars);
   }
   
   // Replace funny kanji characters in section name
   $vars['body_classes'] = str_replace('-e6-bc-a2-e5-ad-97-e6-84-9f-e3-81-98', 'kanjikanji', $vars['body_classes']);
-  
 }
 
 function fly_preprocess_page(&$vars) {
-  $vars['user_avatar'] = _make_avatar_thumb($vars['user']->name, $vars['user']->picture);
+  $vars['user_avatar'] = _fly_make_avatar_thumb($vars['user']->name, $vars['user']->picture);
 }
 
 function fly_preprocess_node(&$vars) {
@@ -67,7 +85,7 @@ function fly_preprocess_node(&$vars) {
   }
   
   // Format nice blog calendar style dates
-  $vars['blog_date'] = _make_blog_date($vars['node']->created);
+  $vars['blog_date'] = _fly_make_blog_date($vars['node']->created);
 
    // To access regions in nodes
    $vars['node_top'] = theme('blocks', 'node_top');
@@ -81,7 +99,7 @@ function fly_preprocess_node(&$vars) {
 function fly_preprocess_comment(&$vars) {
   
   // sets avatar image
-   $vars['picture'] = _make_avatar_thumb($vars['comment']->name, $vars['comment']->picture);
+   $vars['picture'] = _fly_make_avatar_thumb($vars['comment']->name, $vars['comment']->picture);
   
   //show links?
   if( user_access('administer comments') || ($vars['comment']->name == $vars['user']->name)){
