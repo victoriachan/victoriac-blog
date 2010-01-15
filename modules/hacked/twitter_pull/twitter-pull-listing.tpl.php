d0fadccf09a2cd73111ebf0a1c883ea3b463fdad
Victoria Chan <contact@victoriac.net>
2010-01-15 22:09:09 +0000

    Added TWitter Pull module (pre hacking)

---
+++ modules/hacked/twitter_pull/twitter-pull-listing.tpl.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+  Available variables in the theme include:
+  
+  1) an array of $tweets, where each tweet object has:
+      $tweet->id
+      $tweet->username
+      $tweet->userphoto
+      $tweet->text
+      $tweet->timestamp
+      
+  2) $twitkey string containing initial keyword.
+  
+  3) $title
+
+*/
+
+drupal_add_css (drupal_get_path('module', 'twitter_pull') . '/twitter-pull-listing.css');
+
+?>
+
+<div class="tweets-pulled-listing">
+  
+  <?php if (!empty($title)): ?>
+    <h2><?php print $title; ?></h2>
+  <?php endif; ?>
+  
+  <?php if (is_array($tweets)): ?>
+    <ul class="tweets-pulled-listing">
+    <?php foreach ($tweets as $tweet):  ?>
+      <li>
+        <div class="tweet-authorphoto"><img src="<?php print $tweet->userphoto; ?>"></div>
+        <span class="tweet-author"><?php print l($tweet->username, 'http://twitter.com/' . $tweet->username); ?></span>      
+        <span class="tweet-text"><?php print twitter_pull_add_links($tweet->text); ?></span>
+        <div class="tweet-time"><?php print l(format_interval(time() - $tweet->timestamp) . ' ' . t('ago'), 'http://twitter.com/' . $tweet->username . '/status/' . $tweet->id);?></div>   
+        <div class="tweet-divider"></div>  
+      </li>
+    <?php endforeach; ?>
+    </ul>
+  <?php endif; ?>
+</div>
